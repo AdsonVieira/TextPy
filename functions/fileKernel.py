2cc990d03d4537563bbb4d445fc89ea2b4f4e416
Adson Vieira <adsonvieira@hnkweb.com.br>
2017-10-08 02:17:02 -0300

    Atualizações

--- functions/fileKernel.py
+++ functions/fileKernel.py
@@ -1,5 +1,5 @@
 import os.path
-
+import sys
 
 def fileCreate(strFile): 
     if os.path.isfile('documents/'+strFile+".txt"):
@@ -26,10 +26,26 @@ def textpy(strFile):
         print('Esse arquivo não existe')
 
 def selection_sort(strFile):
+
+    palavras_recorrentes = ['gastronomia', 'culinária', 'gastronômico',
+                            'restaurante', 'cozinha', 'cardápios', 
+                            'ingredientes', 'bebidas']
+
     if verifyFile(strFile):
         fl = open('documents/'+strFile+".txt", "r")
-        listtxt = fl.read().split(' ')
+        listtxt = fl.read()
+        #removendo caractesres 
+        cesp = ",.-#@–():"
+        for i in range(0,len(cesp)):
+            listtxt=listtxt.replace(cesp[i],"")
 
+        #deixando tudo minúsculo
+        listtxt = listtxt.lower()
+
+        #transformando em array
+        listtxt = listtxt.split()
+
+        
         for i in range(0, len(listtxt)-1):
             imin = i
             for j in range(i+1, len(listtxt)):
@@ -38,7 +54,28 @@ def selection_sort(strFile):
             aux = listtxt[imin]
             listtxt[imin] = listtxt[i]
             listtxt[i] = aux
-        print(listtxt)
+
+        dic_palavras = {}
+        for i in range(0, len(listtxt)-1):
+            chave = listtxt[i]
+
+            if chave in dic_palavras:
+                dic_palavras[listtxt[i]] += 1
+            else:
+                dic_palavras[listtxt[i]] =1
+             
+
+        listtxt = "\n".join(str(x) for x in listtxt)
+        list_ocor = "Número de ocorrencias\n" + "\n".join("{}: {}".format(k, v) for k, v in dic_palavras.items()) + ""
+
+        #salvando no arquivo por ordem de ocorrencia
+        palavras= open('documents/palavras.txt', "w")
+        palavras.write(listtxt)
+        palavras.close()
+
+        ocorrencia= open('documents/ocorrencia.txt', "w")
+        ocorrencia.write(list_ocor)
+        ocorrencia.close()
 
 def verifyFile(strFile):
     if os.path.isfile('documents/'+strFile+".txt"):
